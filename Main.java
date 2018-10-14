c5a9253fb3f8a1e4417deae8ef15d8cfc49a3cfd
hung99 <37543543+17020782@users.noreply.github.com>
2018-10-14 23:55:23 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package sample;
+
+
+// test demo huy va hung
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandline test = new DictionaryCommandline();
+        test.dictionaryBasic();
+        test.dic_mana.dictionarySearcher();
+        test.dic_mana.addWord();
+        test.dic_mana.editWord();
+    }
+}
